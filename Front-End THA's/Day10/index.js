a30b222223a0c1a5916df0da3355d66ec44d4276
Nirbhay Agrawal <agrawalnirbhay@yahoo.com>
2021-07-31 19:41:22 +0530

    Added Missing Tha's

---
+++ Front-End THA's/Day10/index.js
@@ -0,0 +1,134 @@
+const grid = document.getElementById("grid");
+let last = [];
+let temp = 0;
+let moves = 0;
+let scores = 0;
+let emojiArray = [
+  "🤑",
+  "😲",
+  "🥵",
+  "😱",
+  "😂",
+  "🍆",
+  "🍕",
+  "🍔",
+  "🍟",
+  "🥭",
+  "🍎",
+  "🌽",
+  "🍅",
+  "🌷",
+  "🤯",
+  "🤡",
+  "👺",
+  "👾",
+  "🤑",
+  "😲",
+  "🥵",
+  "😱",
+  "😂",
+  "🍆",
+  "🍕",
+  "🍔",
+  "🍟",
+  "🥭",
+  "🍎",
+  "🌽",
+  "🍅",
+  "🌷",
+  "🤯",
+  "🤡",
+  "👺",
+  "👾",
+];
+const moveDiv = document.getElementById("result");
+const scoreDiv = document.getElementById("score");
+emojiArray = shuffle(emojiArray);
+
+for (let i = 0; i < 36; i++) {
+  let div = document.createElement("div");
+  let front = document.createElement("div");
+  let rear = document.createElement("div");
+  front.className = "front";
+  rear.className = "rear";
+  div.className = "tile";
+
+  front.innerHTML = "<p>♦</p>";
+  rear.innerHTML = `<p>${emojiArray[i]}</p>`;
+  rear.id = `${i}`;
+  grid.appendChild(div);
+
+  div.appendChild(front);
+  div.appendChild(rear);
+}
+let random = function (e) {
+  if (!e.target.classList.contains("spin") && temp < 2) {
+    e.target.classList.add("spin");
+    temp += 1;
+    let rear = e.target.childNodes[1];
+    if (last.length > 0) {
+      if (
+        rear.childNodes[0].innerHTML === last[1].childNodes[0].innerHTML &&
+        last[1].id != rear.id
+      ) {
+        e.target.classList.add("disabled");
+        last[0].classList.add("disabled");
+        scores += 1;
+        temp = 0;
+        scoreDiv.innerText = `Score: ${scores}`;
+        last = [];
+      } else {
+        removeSpin(e, last[0]);
+        last = [];
+      }
+    } else {
+      last = [e.target, rear];
+    }
+
+    moves += 1;
+    moveDiv.innerText = `Moves: ${moves}`;
+    if (scores >= 18) {
+      gameOver();
+    }
+  }
+};
+grid.addEventListener("click", random);
+
+function shuffle(array) {
+  var currentIndex = array.length,
+    randomIndex;
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+
+function removeSpin(e, passedLast) {
+  setTimeout(() => {
+    e.target.classList.remove("spin");
+    passedLast.classList.remove("spin");
+    temp = 0;
+  }, 800);
+}
+function gameOver() {
+  grid.innerHTML = "<h1>Game Over You Won!</h1>";
+  let button = document.createElement("button");
+  button.classList.add("end-btn");
+  grid.removeEventListener("click", random);
+  button.addEventListener("click", () => {
+    location.reload();
+    return false;
+  });
+
+  button.innerText = "reload";
+  grid.appendChild(button);
+  grid.classList.add("game-over");
+}
