98b43ff8cb408ae204a9fbc7028e00c6e8d1c497
Nirbhay Agrawal <agrawalnirbhay@yahoo.com>
2021-07-31 19:20:44 +0530

    Added Day 23

--- Front-End THA's/Day23(React Day9)/custom-hooks/src/App.js
+++ Front-End THA's/Day23(React Day9)/custom-hooks/src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import useToggle from "./useToggle";
 function App() {
+  const [isToggled, setToggle] = useToggle(false);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={setToggle}>
+          {isToggled ? "Hello" : "Click this button"}
+        </button>
       </header>
     </div>
   );
